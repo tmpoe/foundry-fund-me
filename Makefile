0ee959372344ed0543445463899a150edec6224a
tmpITK <torok.mark.patrik@gmail.com>
2023-07-03 16:59:25 +0200

    Add fund script

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 install-dev:
 	forge install OpenZeppelin/openzeppelin-contracts --no-commit; \
-	forge install smartcontractkit/chainlink-brownie-contracts --no-commit
+	forge install smartcontractkit/chainlink-brownie-contracts --no-commit; \
+	forge install Cyfrin/foundry-devops --no-commit; \
---
+++ script/Interactions.s.sol
@@ -0,0 +1,22 @@
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract Fund is Script {
+    uint256 private constant FUND_AMOUNT = 0.001 ether;
+
+    function fund(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: FUND_AMOUNT}();
+        vm.stopBroadcast();
+        console.log("Funded");
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        fund(mostRecentlyDeployed);
+    }
+}
